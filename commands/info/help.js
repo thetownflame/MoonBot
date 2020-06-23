c9202aa5c871986eb67e90080995ab43789320b4
ricochan (c) 2020 <65355430+thetownflame@users.noreply.github.com>
2020-06-23 12:03:43 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -9,14 +9,14 @@ module.exports = {
   async execute(client, message, args) {
 
       const embed = new Discord.MessageEmbed()
-      .setAuthor('Moonlight |  Команды', 'https://images-ext-2.discordapp.net/external/Z8jSSTkQ8AKxnK6m7qhOIX8nNR9U-FLVkXGv7cXaEEs/https/cdn.discordapp.com/avatars/719664504203640975/bdd04cfdb5bb773e3737f9488ff47ca3.webp', 'https://discord.com/oauth2/authorize?client_id=719664504203640975&scope=bot&permissions=2054351998')
-      .setColor("FBFF00")
-      .setDescription(`Это весь список доступных мне команд\nПрефикс бота: ${PREFIX}`)
-      .addField(`Fun commands:`, " ``8ball``, ``cat``, ``cuddle``, ``dice``, ``dog``, ``flip``\n``hug``, ``kiss``, ``pat``, ``poke``, ``slap``, ``tickle``")
-      .addField(`Music commands:`, " ``play``, ``pause``, ``resume``, ``skip``, ``stop``, ``loop``")
-      .addField(`Help commands:`, " ``animesearch``, ``avatar``, ``calc``, ``invite``, ``osu``, ``server``, ``user``, ``weather``, ``steam``")
-      .addField(`Moderation commands:`, " ``ban``, ``kick``")
-
+      .setAuthor('Moonlight |  Команды', 'https://images-ext-2.discordapp.net/external/TkFBlaEeVGw6OJednU0-LGGjUInZz04xvGB2w-f06PE/https/cdn.discordapp.com/avatars/719664504203640975/e55288a0d1ac54c31fbcaa8e4ff92f1a.webp', 'https://discord.com/oauth2/authorize?client_id=719664504203640975&scope=bot&permissions=2054351998')
+      .setColor("FFFAFA")
+      .setDescription(`<:kannawave:723501163001151569> Все доступные команды\nПрефикс бота: ${PREFIX}`)
+      .addField(`**Fun commands**`, " ``8ball``, ``cat``, ``cuddle``, ``dice``, ``dog``, ``flip``\n``hug``, ``kiss``, ``pat``, ``poke``, ``slap``, ``tickle``")
+      .addField(`**Music commands**`, " ``play``, ``pause``, ``resume``, ``skip``, ``stop``, ``loop``")
+      .addField(`**Game commands**`, " ``osu``, ``steam``")
+      .addField(`**Info commands**`, " ``animesearch``, ``avatar``, ``calc``, ``invite``, ``server``, ``user``, ``weather``, ``stats``")
+      .addField(`**Moderation commands**`, " ``ban``, ``kick``")
 
       message.channel.send(embed);
 
