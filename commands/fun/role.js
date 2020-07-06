54858f38cf6b508b80d616883bd1b4390868452c
ricochan (c) 2020 <65355430+thetownflame@users.noreply.github.com>
2020-07-06 19:26:18 +0300

    Update 1.3

--- commands/fun/role.js
+++ commands/fun/role.js
@@ -18,6 +18,7 @@ module.exports = {
                 .setThumbnail(message.guild.iconURL)
                 .setTitle('Информация о роли')
                 .addField(':arrow_right: Название', role.name, true)
+                .addField(':arrow_right: Цвет', role.hexColor, true)
                 .addField(':arrow_right: ID', role.id, true)
                 .addField(':arrow_right: Дата создания', role.createdAt.toDateString(), true)
                 .addField(':arrow_right: Отображаемая отдельно', role.hoist ? 'Да' : 'Нет', true)
